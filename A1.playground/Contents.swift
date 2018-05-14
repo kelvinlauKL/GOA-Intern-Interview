6742121aa7a552d4c4950e2446cb7730d86192cf
Paul Tang <paultang@Pauls-Air.hitronhub.home>
2018-05-13 21:57:51 -0700

    changed A1

--- A1.playground/Contents.swift
+++ A1.playground/Contents.swift
@@ -14,29 +14,26 @@ import UIKit
  }
  return -1
  }
- 
- func firstDuplicatedElement(in array: [Int]) -> Int? {
- 
- for firstNumber in array.count {
- 
- for secondNumber in (firstNumber + 1)...array.count {
- 
- if array(firstNumber) == array(secondNumber) {
- 
- return firstNumber
- }
- }
- }
- return -1
- }
- 
- let testArray = [2, 4, 5, 1, 3, 5, 4]
- 
- if let duplicate = firstDuplicatedElement(in: testArray) {
- print(duplicate) // prints 4
- }
  */
-
-for _ in 0..<3 {
-    print("Hello three times")
+func firstDuplicatedElement(in array: [Int]) -> Int? {
+    
+    for firstNumber in stride(from: 0, to: array.count, by: 1)  {
+        
+        for secondNumber in stride(from: firstNumber + 1, to: array.count, by: 1) {
+            
+            if array[firstNumber] == array[secondNumber] {
+                
+                return array[firstNumber]
+            }
+        }
+        
+    }
+    return nil
 }
+
+let testArray = [2, 4, 5, 1, 2]
+
+if let duplicate = firstDuplicatedElement(in: testArray) {
+    print(duplicate)
+}
+ 
