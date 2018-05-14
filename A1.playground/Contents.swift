e5b8cd0b16ba9ca864c11220bfc98882f6edc9c5
Paul Tang <paultang@Pauls-Air.hitronhub.home>
2018-05-13 21:44:58 -0700

    added A1 and A2

---
+++ A1.playground/Contents.swift
@@ -0,0 +1,42 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+/*
+ func firstDuplicatedElement(in array: [Int]) -> Int? {
+ for firstNumber in array.count {
+ for secondNumber in stride(from: firstNumber + 1, to: array.count, by: 1) {
+ if array(firstNumber) == array(secondNumber) {
+ return firstNumber
+ }
+ }
+ }
+ return -1
+ }
+ 
+ func firstDuplicatedElement(in array: [Int]) -> Int? {
+ 
+ for firstNumber in array.count {
+ 
+ for secondNumber in (firstNumber + 1)...array.count {
+ 
+ if array(firstNumber) == array(secondNumber) {
+ 
+ return firstNumber
+ }
+ }
+ }
+ return -1
+ }
+ 
+ let testArray = [2, 4, 5, 1, 3, 5, 4]
+ 
+ if let duplicate = firstDuplicatedElement(in: testArray) {
+ print(duplicate) // prints 4
+ }
+ */
+
+for _ in 0..<3 {
+    print("Hello three times")
+}
