09331c0ae0bfc4c405e368006ac5c6b8806e4347
HodKimhi <39248458+HodKimhi@users.noreply.github.com>
2018-05-13 19:02:09 -0700

    Removing unecessary comments

--- ArrayDuplicate.playground/Contents.swift
+++ ArrayDuplicate.playground/Contents.swift
@@ -11,17 +11,10 @@ func firstDuplicatedElement(in array: [Int]) -> Int? {
         for n in i+1..<testArray.count{ //for loop to compare every element after the previous one for duplicates
             if(testArray[i] == testArray[n]){ //if element i == element n, we have a duplicate
                 result = testArray[n]; //define result as the duplicated element so we can return it later
-                //print("Success")
-                /* compares the two values (should be same) */
-                //print(testArray[i], testArray[n])
                 return result //return the duplicated element
-            }else{ //if element i != element n (not a duplicate)
-                /* just to see the values of i an n */
-                //print("i is: \(testArray[i])")
-                //print("n is: \(testArray[n])")
+            }
             }
         }
-        //print("\n")
     }
     
     return result //by default, result is nil so this will return nil
