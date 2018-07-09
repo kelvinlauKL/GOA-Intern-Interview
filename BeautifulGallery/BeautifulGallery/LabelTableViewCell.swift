e21eea8a11574cc9e73aeeef33ad9347bffa7b71
Martin Yushko <martin.yushko@gameofapp.org>
2018-07-09 15:38:24 -0700

    Added sorting funcitonality to the table view

---
+++ BeautifulGallery/BeautifulGallery/LabelTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  LabelTableViewCell.swift
+//  BeautifulGallery
+//
+//  Created by Martin Yushko on 2018-07-09.
+//  Copyright © 2018 MartinYushko. All rights reserved.
+//
+
+import UIKit
+
+class LabelTableViewCell: UITableViewCell {
+
+    
+    @IBAction func showButton(_ sender: Any) {
+        
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+       
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+}
--- BeautifulGallery/BeautifulGallery/ViewController.swift
+++ BeautifulGallery/BeautifulGallery/ViewController.swift
@@ -10,13 +10,23 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tb: UITableView!
+    
+    var masterFood: [ModelData] = []
+    
   struct ModelData{
+    enum Food {
+        case fruit
+        case dessert
+    }
+    var type: Food
     var name: String
     var image: UIImage?
     let description: String = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-    init(name: String, image: UIImage){
+    init(name: String, image: UIImage, foodType: Food){
       self.name = name
       self.image = image
+      self.type = foodType
     }
   }
   
@@ -25,14 +35,18 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
   
   
   public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return (food.count)
+    return (food.count+1)
   }
   
   public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    if(indexPath.row == 0){
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell2", for: indexPath) as! LabelTableViewCell
+        return cell
+    }
     let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CustomTableViewCell
-    cell.cellImage.image = food[indexPath.row].image
-    cell.titleLabel.text = food[indexPath.row].name
-    cell.descriptionLabel.text = food[indexPath.row].description
+    cell.cellImage.image = food[indexPath.row-1].image
+    cell.titleLabel.text = food[indexPath.row-1].name
+    cell.descriptionLabel.text = food[indexPath.row-1].description
     return cell
   }
   
@@ -42,23 +56,65 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
   @IBOutlet var expandedView: UIView!
   @IBOutlet weak var expandedLabel: UILabel!
   @IBOutlet weak var dimView: UIView!
+    
+    @IBOutlet var selectionVeiw: UIView!
+    
+    @IBAction func allFoodsSelection(_ sender: Any) {
+    food = masterFood
+    tb.reloadData()
+    selectionVeiw.removeFromSuperview()
+    }
+    
+    
+    @IBAction func fruitsOnlySelection(_ sender: Any) {
+        food.removeAll()
+        for i in 0..<masterFood.count{
+            if(masterFood[i].type == .fruit){
+                food.append(masterFood[i])
+            }
+        }
+        tb.reloadData()
+        selectionVeiw.removeFromSuperview()
+    }
+    
+    @IBAction func dessertsOnlySelection(_ sender: Any) {
+        food.removeAll()
+        for i in 0..<masterFood.count{
+            if(masterFood[i].type == .dessert){
+                food.append(masterFood[i])
+            }
+        }
+        tb.reloadData()
+        selectionVeiw.removeFromSuperview()
+    }
+    
+    
+    
+    
   
   public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-    let data = food[indexPath.row]
+    if(indexPath.row == 0) {
+        //selectionVeiw.layer.cornerRadius = 20
+        let centerO = (tableView.cellForRow(at: indexPath)?.center)!
+        selectionVeiw.center = CGPoint(x: centerO.x - CGFloat(85), y: centerO.y + CGFloat(190))
+        view.addSubview(selectionVeiw)
+        
+    }else{
+        let data = food[indexPath.row-1]
     
-    expandedImage.image = data.image
-    expandedLabel.text = data.name
-    expandedView.center = view.center
+        expandedImage.image = data.image
+        expandedLabel.text = data.name
+        expandedView.center = view.center
     
-    expandedView.transform = CGAffineTransform(scaleX: 0.8, y: 1.2)
+        expandedView.transform = CGAffineTransform(scaleX: 0.8, y: 1.2)
     
-    view.addSubview(expandedView)
+        view.addSubview(expandedView)
 
-    UIView.animate(withDuration: 0.3, animations: {self.dimView.alpha = 0.8})
-    UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0, options: [], animations: {
-          self.expandedView.transform = .identity
-    })
-    
+       // UIView.animate(withDuration: 0.3, animations: {self.dimView.alpha = 0.8})
+       // UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.5,  initialSpringVelocity: 0, options: [], animations: {
+          //  self.expandedView.transform = .identity
+      //  })
+    }
   }
   
   @IBAction func closeExpandedView(_ sender: Any) {
@@ -75,13 +131,13 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    food.append(ModelData(name: "Banana", image: UIImage(named: "banana")!))
-    food.append(ModelData(name: "Apple", image: UIImage(named: "apple")!))
-    food.append(ModelData(name: "Orange", image: UIImage(named: "orange")!))
-    food.append(ModelData(name: "Chocolate", image: UIImage(named: "chocolate")!))
-    food.append(ModelData(name: "Cake", image: UIImage(named: "cake")!))
-    food.append(ModelData(name: "Strawberry", image: UIImage(named: "strawberry")!))
-
+    food.append(ModelData(name: "Banana", image: UIImage(named: "banana")!, foodType: .fruit))
+    food.append(ModelData(name: "Apple", image: UIImage(named: "apple")!, foodType: .fruit))
+    food.append(ModelData(name: "Orange", image: UIImage(named: "orange")!, foodType: .fruit))
+    food.append(ModelData(name: "Chocolate", image: UIImage(named: "chocolate")!, foodType: .dessert))
+    food.append(ModelData(name: "Cake", image: UIImage(named: "cake")!, foodType: .dessert))
+    food.append(ModelData(name: "Strawberry", image: UIImage(named: "strawberry")!, foodType: .fruit))
+    masterFood = food
     expandedButton.layer.cornerRadius = expandedButton.frame.width/2
     expandedView.layer.cornerRadius = 16
     
